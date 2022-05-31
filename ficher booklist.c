4069df747e685cf2ef626684b5377cbc8c390d4a
narvaest <105703270+narvaest@users.noreply.github.com>
2022-05-31 09:46:07 +0200

    Add files via upload

---
+++ ficher booklist.c
@@ -0,0 +1,126 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>  
+
+
+typedef struct{
+	char* title;
+	char* writer; 
+	int number;
+	char* category;
+}Book;
+
+void flushScanf(){
+	int res;
+	char c;
+	do{
+		res = scanf("%c", &c);
+	} while( res==1 && c!='\n' );
+}	
+
+
+Book* readBook(){
+	char tmp[256];
+	char top[256];
+	char tkp[256];
+	Book* struc = NULL;
+	FILE* paper = NULL;
+	paper = fopen("Booklist.txt", "r+");
+	if(paper == NULL){
+		puts("Erreur d'allocation");
+		printf("%d : %s", errno, strerror(errno));
+		exit(1);
+	}
+	struc = malloc(5 * sizeof(Book));
+	if(struc == NULL){
+		exit(2);
+	}
+	for(int i = 0; i < 5; i++){
+		fscanf(paper, "%s", tmp);
+		int N = strlen(tmp);
+		struc[i].title = malloc( (N+1) * sizeof(char) );
+		if(struc[i].title == NULL){
+			puts("Erreur d'allocation");
+			exit(3);
+		}
+	    strcpy(struc[i].title , tmp);
+	    
+		fscanf(paper, "%s", top);
+		int M = strlen(top);
+		struc[i].writer = malloc( (M+1) * sizeof(char) );
+		if(struc[i].writer == NULL){
+			puts("Erreur d'allocation");
+			exit(4);
+		}
+	    strcpy(struc[i].writer , top);
+	    
+	    fscanf(paper, "%s", tkp);
+		int K = strlen(tkp);
+		struc[i].category = malloc( (K+1) * sizeof(char) );
+		if(struc[i].category == NULL){
+			puts("Erreur d'allocation");
+			exit(5);
+		}
+	    strcpy(struc[i].category , tkp);
+	    
+		fscanf(paper, "%d", &(struc[i].number));
+	}
+	
+	fclose(paper);
+	return struc;
+}
+
+void newbooks(){
+	FILE * news = NULL;
+	news = fopen("Booklist.txt", "a");
+	if(news == NULL){
+		puts("Erreur d'allocation");
+		exit(1);
+	}
+	char tit[50]; 
+	char wri[50]; 
+	int nmb = 0;
+	char cat[50];
+	
+	puts("Quel est votre livre?");
+	scanf("%s", tit);
+	flushScanf();
+	//scanf("%s", log);
+	fprintf(news, "%s\t", tit);
+	
+	
+	puts("Quel est l'auteur?");
+	scanf("%s", wri);
+	flushScanf();
+	fprintf(news, "%s\t", wri);
+	
+	puts("Quel est la catégorie?");
+	scanf("%s", cat);
+	flushScanf();
+	fprintf(news, "%s\t", cat);
+	
+	do{
+		puts("Quel est le numéro?");
+		scanf("%d", &nmb);
+		flushScanf();
+	}while(nmb <= 0);
+	fprintf(news, "%d\n", nmb);
+	
+
+	fclose(news);
+}
+
+
+
+int main()
+{
+	Book* tab; 
+	tab = readBook();
+	for(int i; i<3; i++){
+		printf("Livre %d : %s de %s \n", i+1 , tab[i].title, tab[i].writer); 
+	}
+	newbooks();
+	return 0;
+}
+
--- fichier userlist.c
+++ fichier userlist.c
@@ -3,6 +3,8 @@
 #include <string.h>
 #include <errno.h>  
 
+	
+
 typedef struct{
 	char* login;
 	char* password;
@@ -10,10 +12,17 @@ typedef struct{
 	char* borrow[20];
 }Username;
 
-
+	
+void flushScanf(){
+	int res;
+	char c;
+	do{
+		res = scanf("%c", &c);
+	} while( res==1 && c!='\n' );
+}	
 
 //menu d'utilisation et choix
-int menu(){
+int menuStudent(){
 	puts("Quelle action voulez vous faire?");
 	puts("1.Emprunter un livre");
 	puts("2.Rendre un livre");
@@ -21,7 +30,22 @@ int menu(){
 	int res = 0;
 	do{
 		scanf("%d", &res);
+		flushScanf();
 	}while(res > 3 || res <= 0);
+	return res;
+}
+
+int menuTeacher(){
+	puts("Quelle action voulez vous faire?");
+	puts("1.Emprunter un livre");
+	puts("2.Rendre un livre");
+	puts("3.Se déconnecter");
+	puts("4.Ajouter un livre");
+	int res = 0;
+	do{
+		scanf("%d", &res);
+		flushScanf();
+	}while(res > 4 || res <= 0);
 	return res;
 }
 
@@ -32,10 +56,11 @@ int connection(){
 	int res = 0;
 	do{
 		scanf("%d", &res);
+		flushScanf();
 	}while(res > 2 || res <= 0);
-	printf("%d", res);
 	return res;
 }
+
 
 /*
  declaration tableau 'tmp' 256 char
@@ -63,7 +88,7 @@ Username* readFile(){
 	if(struc == NULL){
 		exit(2);
 	}
-	for(int i = 0; i < 5; i++){
+	for(int i = 0; i < 2; i++){
 //		fscanf(user, "%s", struc[i].login);
 		fscanf(user, "%s", tmp);
 		int N = strlen(tmp);
@@ -91,33 +116,40 @@ Username* readFile(){
 
 	
 	
-	
 void newusers(){
 	FILE * new = NULL;
-	new = fopen("userslist.txt", "r+");
+	new = fopen("userslist.txt", "a");
 	if(new == NULL){
 		puts("Erreur d'allocation");
 		exit(1);
 	}
-	fseek(new, 0, SEEK_END);
-	if(new == NULL){
-		puts("Erreur d'allocation");
-		exit(2);
-	}
-	char* log = NULL; 
-	char* pw = NULL; 
-//	int* trk = NULL;
+	char log[20]; 
+	char pw[20]; 
+	int trk = 0;
 	
 	//SEGMENTATION FAULT
 	
 	puts("Quel est votre login?");
 	scanf("%s", log);
-	fprintf(new, "\n %s", log);
+	flushScanf();
+	//scanf("%s", log);
+	fprintf(new, "%s\t", log);
+	
+	
 	puts("Quel est votre mot de passe?");
 	scanf("%s", pw);
-	fprintf(new, "   %s   ", pw);	
-//	puts("Quel est votre profession ? (1 = élèves, 2 = professeur)");
-//	scanf("%d", trk);
+	flushScanf();
+	fprintf(new, "%s\t", pw);
+	if(new == NULL){
+			puts("Erreur d'allocation");
+			exit(74);
+	}
+	do{
+		puts("Quel est votre profession ? (1 = élèves, 2 = professeur)");
+		scanf("%d", &trk);
+		flushScanf();
+	}while(trk > 2 || trk <= 0);
+	fprintf(new, "%d\n", trk);
 	fclose(new);
 }
 
@@ -126,12 +158,14 @@ int main()
 {
 	Username* tab;
 	tab = readFile();
-	printf("%d", tab[1].type);
+	printf("%d \n", tab[1].type);
 	int choice = connection();
-	if(choice == 2){
-		
+	if(choice == 2){	
 		newusers();
 	}	
 	return 0;
 }
 
+//Faire un systeme de connexion
+//Si la personne est professeur, il peut ajouter un livre 
+//Faire un système d'emprunt de livre et pour rendre le lire 
